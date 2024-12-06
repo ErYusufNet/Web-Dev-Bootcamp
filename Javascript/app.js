46fc6fc4e588ae5460bd69400a46d2c6c1e4726b
eryusufnet <yusufer.net@hotmail.com>
2024-12-06 10:13:07 +0200

    up

--- Javascript/app.js
+++ Javascript/app.js
@@ -1,1 +1,11 @@
-document.el
+
+/*document.write("Cikti almak ister misiniz? <br> ");
+document.writeln("Bu bir cikti yontemi O yuzden Hello world diyorum");
+document.write(55);
+console.log("Hi whats up");
+
+let a =5;
+let b=3;
+console.log("iki sayinin toplami : ", a+b);*/
+alert("Hello welcome");
+alert("hi");
